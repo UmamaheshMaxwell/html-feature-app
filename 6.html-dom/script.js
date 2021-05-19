3376625ead1b287575750060dec1d44566fe300b
Umamaheswararao Meka <maheshmeka16@gmail.com>
2021-05-19 11:24:02 +0530

    added dom, audio, video, sematic elements

---
+++ 6.html-dom/script.js
@@ -0,0 +1,9 @@
+document.getElementById("header").innerHTML = "This header text coming from Javascript"
+document.getElementById("para").innerHTML ="This para is coming from JavScript"
+
+function editDOMControl(){
+    document.getElementById("header").style.backgroundColor = "crimson";
+    document.getElementById("header").style.color ="white"
+    document.getElementById("firstName").value = "Hello"
+}
+
